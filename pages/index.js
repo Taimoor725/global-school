71cf3d87c5a58131a868176a03bcaa3f076fe0f7
Taimoor Tanveer <taimoor725tanveer@gmail.com>
2025-02-13 02:42:32 +0500

    correction 1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/navigation";
 export default function Home() {
   const router=useRouter();
+  router.push('/home')
   return (
-   router.push('/home')
+   <></>
   );
 }
