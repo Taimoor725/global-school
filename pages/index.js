46fb9777af8464ff566eedb2c2c37373fcdd2f5a
Taimoor Tanveer <taimoor725tanveer@gmail.com>
2025-02-13 02:38:31 +0500

    correction

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-
-// import Navbar from "@/components/common/NavBar";
-// import FeedbackList from "../components/FeedbackList";
-
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router=useRouter();
   return (
-    <div>
-      {/* <Navbar/> */}
-      <div className="container mx-auto p-4">
-        <h2 className="text-xl font-bold mb-4">Recent Feedbacks</h2>
-        <FeedbackList />
-      </div>
-    </div>
+   router.pust('/home')
   );
 }
